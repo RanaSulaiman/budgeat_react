ddef4b9cef69b8002d0b57870682bb634fe3e06b
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-12 19:13:43 -0700

    succeeded in rendering recipe info from rails api, WIP to fix bug/image

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react'
-
-// import {fetchRailsData} from '../utils/RailsApi'
-import FilterRecipes from './FilterRecipes'
-import RecipeRow from './RecipeRow'
+import axios from 'axios'
 
 class RecipeInfo extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: {},
+    }
+  }
+
+  componentDidMount(){
+    let recipe_id = this.props.match.params.recipe_id;
+
+    axios.get('http://localhost:4000/recipes/' + recipe_id)
+      .then((response)=>{
+        const recipe = response.data;
+          this.setState({ recipe });
+          console.log('getRecipeInfoById: Successful API call');
+          console.log(this.state.table);
+        })
+        .catch((error)=>{
+          console.log('getRecipeInfoById: Failed API call');
+          console.log(error);
+        })
+
+  }
 
   render() {
-    return (
-      <div>
-        {/* <p>{this.props.match.params.recipe_id}</p> */}
+    let recipe_id = this.props.match.params.recipe_id;
+
+    return(
+        <div>
+          <h1>Detail for recipe # {recipe_id}</h1>
+          <h1>{this.state.recipe.title}</h1>
+          <h1>{this.state.recipe.sourceName}</h1>
+          <h1>{this.state.recipe.image}</h1>
+          <p>{this.state.recipe.instructions}</p>
 
 
-      </div>
-
-
+          {/* <h1>{this.state.table.cuisines[0]}</h1> */}
+        </div>
     );
   }
 }
