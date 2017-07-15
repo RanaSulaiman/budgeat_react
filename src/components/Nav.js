edd353e24e8c03ae0b5908c5fd5ded45b9a6def4
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-15 15:17:04 -0700

    Still can't import local images to react componenet

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,18 @@ import {Link} from 'react-router-dom'
 import Home from './Home'
 import Contact from './Contact'
 import FilterRecipes from './FilterRecipes'
+// import home from '../images/home.png';
+
+// console.log(home);
 
 export const Nav = () => (
   <div className='container'>
   <ul className='row'>
     <li className='col-sm-2'>
-      <Link activeClassName='active' to='/'>Home</Link>
+      <Link activeClassName='active' to='/'>
+      Home</Link>
+      {/* <img className='home' height='50'  src={'home'} alt='home'/> */}
+
     </li>
     <li className='col-sm-2'>
       <Link activeClassName='active' to='/Contact'>Contact</Link>
