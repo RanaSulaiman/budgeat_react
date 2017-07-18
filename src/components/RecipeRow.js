cdb87261a759034410c761ded0b3c8755e6b93d2
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-17 23:21:48 -0700

    Styling for title to be centered

--- src/components/RecipeRow.js
+++ src/components/RecipeRow.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 export const RecipeRow = (recipe) => (
   <div>
-    <p className='title'><Link to={'Recipe/' + recipe.recipe_id}>{recipe.title}</Link></p>
+    <p id='title'><Link to={'Recipe/' + recipe.recipe_id}>{recipe.title}</Link></p>
     {/* <p>{recipe.dish_type}</p>
     <p>[{recipe.price_serving} cent] [{recipe.ready_time} min]</p>
     <p>gluten free: {recipe.gluten}</p>
