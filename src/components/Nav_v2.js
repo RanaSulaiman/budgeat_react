4b050c8106d205bdaa4f309bda90d6722b55c639
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-07-24 19:39:13 -0700

    Refactored Responsive mobile header

--- src/components/Nav_v2.js
+++ src/components/Nav_v2.js
@@ -14,21 +14,21 @@ import AttachMoney from 'react-icons/lib/md/attach-money'
 export const Nav = () => (
 
   <div className='container-fluid'>
-    <div className='row-fluid text-align'>
-      <div className='col-lg-4 col-md-5 col-sm-6 col-xs-7 logo-display'>
-        <AttachMoney className='logo-icon'/>
-        <p className='app-name'>BudgEat</p>
-        <AttachMoney className='logo-icon'/>
+    <div className='row'>
+      <div className='logo-display'>
+        <p className='col-lg-2'><AttachMoney className='logo-icon'/></p>
+        <p className='col-lg-2 app-name'>BudgEat</p>
+        <p className='col-lg-2'><AttachMoney className='logo-icon'/></p>
       </div>
 
-      <ul className='col-lg-2 col-md-3 col-md-4 col-xs-5 nav-display' >
-        <li className=''>
+      <ul className='nav-display' >
+        <li className='col-lg-2 text-center'>
           <Link activeClassName='active' to='/'>
           <HomeOutline className='nav-icon'/>
         </Link>
         </li>
 
-        <li className=''>
+        <li className='col-lg-2'>
           <Link activeClassName='active' to='/FilterRecipes'><Search className='nav-icon'/></Link>
         </li>
       </ul>
